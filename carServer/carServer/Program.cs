670488e8c9cf54fb0ec16a6a03390511a677e139
Yopiter <Martinius.schneider@web.de>
2017-05-27 15:17:33 +0000

    Aktuelle Version der C#-Serveranwendung

--- carServer/carServer/Program.cs
+++ carServer/carServer/Program.cs
@@ -4,20 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.IO;
+using System.Diagnostics;
+using System.Threading;
 
 namespace carServer
 {
     class Program
     {
         static TcpClient client;
+		static TcpClient python;
         static BinaryReader biRea;
         static BinaryWriter biWri;
+		static BinaryWriter pyWri;
+		static int HauptR;
+		static int SeitR;
+		static Process py;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Anwendung gestartet!");
-            GetClient();
+			//StartPythonConsole (); Well no thanks
+			GetClient ();
+			while (client.Connected) {
+				GetCommand ();
+			}
             Console.ReadLine();
         }
+
+		static void GetCommand ()
+		{
+			try{
+			Int16 Comm = biRea.ReadInt16 ();
+			switch (Comm) {
+				case(-1): //Rückwärts
+					HauptR--;
+					break;
+				case(1): //Vorwärts
+					HauptR++;
+					break;
+				case(-2): //links
+					SeitR--;
+					break;
+				case(2): //rechts
+					SeitR++;
+					break;
+				case(0): //Do Tara-Stuff
+					SeitR = HauptR = 0;
+					break;
+				case(-3): //Beenden
+					EndApplication();
+					break;
+				default:
+					Console.WriteLine ("Now That's Weird... Command Range +-2");
+					//Rote Lampe blinken lassen
+					break;
+			}
+			}
+			catch(System.IO.IOException e) {
+				python_send (0, 0);
+				Console.WriteLine (e.Message +" bei "+ e.Source+", neuer Client wird gesucht. The Adventure continues!");
+				GetClient ();
+			}
+			DoMotorStuff ();
+		}
+
+		static void DoMotorStuff ()
+		{
+			int leftM, rightM = 0;
+			leftM = HauptR + (HauptR * SeitR);
+			rightM = HauptR - (HauptR * SeitR);
+			if (HauptR == 0) {
+				leftM = SeitR;
+				rightM = -SeitR;
+			}
+			python_send (leftM, rightM);
+		}
+
+		static void python_send (int leftM, int rightM)
+		{
+			byte[] left = IntToBytes (leftM);
+			byte[] right = IntToBytes (rightM);
+			python = new TcpClient ();
+			python.Connect (System.Net.IPAddress.Loopback, 1338);
+			pyWri = new BinaryWriter (python.GetStream ());
+			pyWri.Write (left);
+			pyWri.Write (right);
+			python.Close ();
+		}
 
         static bool GetClient()
         {
@@ -27,6 +100,7 @@ namespace carServer
             client = Listener.AcceptTcpClient();
             biRea = new BinaryReader(client.GetStream());
             biWri = new BinaryWriter(client.GetStream());
+			Listener.Stop ();
             Console.WriteLine("Client gefunden!");
             Random Generator = new Random();
             int Wert = Generator.Next(0, 10);
@@ -51,5 +125,36 @@ namespace carServer
                 return false;
             }
         }
+
+		static void StartPythonConsole () //deprecated since shitty not working shit
+		{
+			Console.WriteLine ("Hole Python-Client");
+			//Prozess starten
+			ProcessStartInfo ProcInfo = new ProcessStartInfo ();
+			ProcInfo.WindowStyle = ProcessWindowStyle.Normal; //Später vielelicht als Hidden?
+			ProcInfo.UseShellExecute = false;
+			ProcInfo.FileName = "python";
+			ProcInfo.Arguments= "/home/pi/car/CarControl.py";
+			ProcInfo.Verb = "";
+			ProcInfo.RedirectStandardOutput = true;
+			py = new Process ();
+			py.StartInfo = ProcInfo;
+			py.Start ();
+			Console.WriteLine ("Python-Skript gestartet!");
+		}
+
+		static byte[] IntToBytes (int MInt)
+		{
+			MInt = Math.Abs (MInt) > 1 ? MInt / Math.Abs (MInt) : MInt;
+			string nachricht=MInt.ToString().Length==1? "+"+MInt.ToString(): MInt.ToString();
+			return Encoding.ASCII.GetBytes (nachricht);
+		}
+
+		static void EndApplication ()
+		{
+			python.Close ();
+			client.Close ();
+			Environment.Exit (0);
+		}
     }
 }
